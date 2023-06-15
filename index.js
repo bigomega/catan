e15a4fc5b42e09afc24b73cd12e6cd74875578fd
Bharath Raja <2320747+bigomega@users.noreply.github.com>
2023-06-15 18:41:43 +0530

    simple express server

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-var app = require('express')();
-var http = require('http').createServer(app);
+const path = require('path')
+const express = require('express')
+const app = express()
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
-});
+app.get('/api', function(req, res){
+  res.send('<h1>Hello world</h1>')
+})
 
+var http = require('http').createServer(app)
 http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
+  console.log('listening on *:3000')
+})
