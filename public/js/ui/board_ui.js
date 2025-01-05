a364b6162776ba67a480a02c9b9837d370c513ef
Bharath Raja ツ <2320747+bigomega@users.noreply.github.com>
2025-01-05 20:08:55 +0530

    Update: Making the render fns of board UI extendable

--- public/js/ui/board_ui.js
+++ public/js/ui/board_ui.js
@@ -3,102 +3,27 @@ import * as CONST from "../const.js"
 const $ = document.querySelector.bind(document)
 const oKeys = Object.keys
 
-const SIZE = { MIN: -7, MAX: 2 }
 export default class BoardUI {
   #board; #onClick;
+  #size = { MIN: -7, MAX: 2 }
+  #renderedCorners = []
+  #renderedEdges = []
   $el = $('#game .board')
 
   /** @param {Board} board  */
-  constructor(board, onClick) {
+  constructor(board, onClick, size) {
     this.#board = board
     this.#onClick = onClick
+    this.#size = size || this.#size
   }
 
   toggleBlur(bool) { this.$el.classList[bool ? 'add' : 'remove']('blur') }
 
   render() {
+    /** @todo fix the variable names base on convention */
     let startDiff = 0
     let maxLeft = 0
     let maxLength = 0
-    const renderedCorners = []
-    const renderedEdges = []
-
-    function _renderCorners(tile) {
-      return oKeys(tile.corners).map(dir => {
-        const corner = tile.corners[dir]
-        // if (corner.id == 4) {debugger}
-        let $_trade = ''
-        if (tile.type === 'S' && tile.trade_edge && corner.trade) {
-          $_trade = `<div class="trade-post p-${dir}"></div>`
-        }
-        if (renderedCorners.includes(corner.id)) { return $_trade }
-        renderedCorners.push(corner.id)
-        return `<div class="corner" data-id="${corner.id}" data-dir="${dir}"></div>
-          ${$_trade}
-        `
-      }).join('')
-    }
-
-    function _renderEdges(tile) {
-      return oKeys(tile.corners).map(dir => {
-        const corner = tile.corners[dir]
-        const relevant_edges = {
-          top: ['left', 'right'],
-          top_left: ['bottom'],
-          top_right: ['bottom'],
-          // bottom_left: ['top', 'right'],
-          // bottom_right: ['top', 'left'],
-          bottom: ['left', 'right'],
-        }[dir]
-        return relevant_edges?.map(e_dir => {
-          const edge = corner.edges[e_dir]
-          if (!edge || renderedEdges.includes(edge.id)) { return '' }
-          renderedEdges.push(edge.id)
-          let css_dir = dir + '-' + e_dir
-          if (e_dir === 'bottom') {
-            css_dir = {
-              'top_left': 'left',
-              'top_right': 'right',
-            }[dir]
-          }
-          return `<div class="edge" data-id="${edge.id}" data-dir="${css_dir}"></div>`
-        }).join('')
-      }).join('')
-    }
-
-    function _renderBeaches(tile) {
-      return oKeys(tile.adjacent_tiles).map(dir =>
-        (tile.adjacent_tiles[dir] && tile.adjacent_tiles[dir].type !== 'S')
-          ? `<div class="beach beach-${Math.floor(Math.random() * 3) + 1} beach-${dir}"></div>`
-          : ''
-      ).join('')
-    }
-
-    function _renderRow(row) {
-      return row.map((tile, j) =>
-        `<div
-          class="tile ${tile.type} ${tile.robbed ? 'robbed' : ''}"
-          data-id="${tile.id}"
-          ${(tile.type === 'S' && tile.trade_edge)
-          ? `data-trade="${tile.trade_type}" data-trade-dir="${tile.trade_edge}"`
-          : ''
-        }
-        >
-          <div class="background"></div>
-          <div class="corners">${_renderCorners(tile)}</div>
-          <div class="edges">${_renderEdges(tile)}</div>
-          ${tile.type === 'S' ? `<div class="beaches">${_renderBeaches(tile)}</div>` : ''}
-          ${tile.num
-          ? `<div
-                class="number ${tile.num > 5 && tile.num < 9 ? 'red' : ''}"
-                num="${tile.num}"
-                dots="${'.'.repeat(6 - Math.abs(7 - tile.num))}">
-              </div>`
-          : ''
-        }
-        </div>`
-      ).join('')
-    }
 
     this.$el.innerHTML = this.#board.tile_rows.map((row, i) => {
       startDiff += (row.diff || 0)
@@ -110,7 +35,7 @@ export default class BoardUI {
           style="left:calc(${startDiff} * var(--tile-width)/2 + ${startDiff * -1}px);
             width: calc(var(--tile-width) * ${row.length})"
         >
-          ${_renderRow(row)}
+          ${this.renderRow(row)}
         </div>
       `
     }).join('')
@@ -118,8 +43,85 @@ export default class BoardUI {
     this.$el.style.paddingLeft = `calc(var(--tile-width) / 2 * ${maxLeft * -1})`
     this.$el.style.width = `calc(var(--tile-width) * ${maxLength})`
     const size = localStorage.getItem('board-size')
-    this.$el.dataset.size = (size >= SIZE.MIN && size <= SIZE.MAX) ? size : 0
+    this.$el.dataset.size = (size >= this.#size.MIN && size <= this.#size.MAX) ? size : 0
     this.#setupEvents()
+  }
+
+  renderRow(row) {
+    return row.map((tile, j) =>
+      `<div
+        class="tile ${tile.type} ${tile.robbed ? 'robbed' : ''}"
+        data-id="${tile.id}"
+        ${(tile.type === 'S' && tile.trade_edge)
+        ? `data-trade="${tile.trade_type}" data-trade-dir="${tile.trade_edge}"`
+        : ''
+      }
+      >
+        <div class="background"></div>
+        <div class="corners">${this.renderCorners(tile)}</div>
+        <div class="edges">${this.renderEdges(tile)}</div>
+        ${tile.type === 'S' ? `<div class="beaches">${this.renderBeaches(tile)}</div>` : ''}
+        ${tile.num
+        ? `<div
+              class="number ${tile.num > 5 && tile.num < 9 ? 'red' : ''}"
+              num="${tile.num}"
+              dots="${'.'.repeat(6 - Math.abs(7 - tile.num))}">
+            </div>`
+        : ''
+        }
+      </div>`
+    ).join('')
+  }
+
+  renderCorners(tile) {
+    return oKeys(tile.corners).map(dir => {
+      const corner = tile.corners[dir]
+      // if (corner.id == 4) {debugger}
+      let $_trade = ''
+      if (tile.type === 'S' && tile.trade_edge && corner.trade) {
+        $_trade = `<div class="trade-post p-${dir}"></div>`
+      }
+      if (this.#renderedCorners.includes(corner.id)) { return $_trade }
+      this.#renderedCorners.push(corner.id)
+      return `<div class="corner" data-id="${corner.id}" data-dir="${dir}"></div>
+        ${$_trade}
+      `
+    }).join('')
+  }
+
+  renderEdges(tile) {
+    return oKeys(tile.corners).map(dir => {
+      const corner = tile.corners[dir]
+      const relevant_edges = {
+        top: ['left', 'right'],
+        top_left: ['bottom'],
+        top_right: ['bottom'],
+        // bottom_left: ['top', 'right'],
+        // bottom_right: ['top', 'left'],
+        bottom: ['left', 'right'],
+      }[dir]
+      return relevant_edges?.map(e_dir => {
+        const edge = corner.edges[e_dir]
+        if (!edge || this.#renderedEdges.includes(edge.id)) { return '' }
+        this.#renderedEdges.push(edge.id)
+        let css_dir = dir + '-' + e_dir
+        if (e_dir === 'bottom') {
+          css_dir = {
+            'top_left': 'left',
+            'top_right': 'right',
+          }[dir]
+        }
+        return `<div class="edge" data-id="${edge.id}" data-dir="${css_dir}"></div>`
+      }).join('')
+    }).join('')
+  }
+
+  renderBeaches(tile) {
+    return oKeys(tile.adjacent_tiles).map(dir =>
+      (tile.adjacent_tiles[dir] && tile.adjacent_tiles[dir].type !== 'S')
+        ? `<div class="beach beach-${Math.floor(Math.random() * 3) + 1} beach-${dir}"></div>`
+        : ''
+    ).join('')
   }
 
   #setupEvents() {
@@ -167,7 +169,7 @@ export default class BoardUI {
 
   toggleZoom(out) {
     const size = (+this.$el.dataset.size || 0) + (out ? -1 : 1)
-    if (size >= SIZE.MIN && size <= SIZE.MAX) {
+    if (size >= this.#size.MIN && size <= this.#size.MAX) {
       this.$el.dataset.size = size
       localStorage.setItem('board-size', size)
     }
